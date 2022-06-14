38d4e26e3969e984656b2f0ab480e4c9d1466696
JuanVera326 <jjvera86@misena.edu.co>
2022-06-14 13:15:07 -0500

    Cambios procedimientos almacenados

--- ejercicioPA.sql
+++ ejercicioPA.sql
@@ -34,29 +34,91 @@ CREATE TABLE ventas(
     foreign key (productos_venta) references productos (id_producto)
 );
 
-create table provee_product(
+create table entregas_inv(
 	id_proveedores varchar (45),
     id_productos varchar (45),
     foreign key (id_proveedores) references proveedores (id_proveedor),
     foreign key (id_productos) references productos (id_producto)
 );
 
-/*-----------------------------------------------------------------------------*/
+/*---------------------------AGREGAR CLIENTE-----------------------------------*/
  DELIMITER $$
 create procedure sp_insertar_cliente(
-	id_cliente varchar(45),
-    nombre_cliente varchar(45),
-    telefono varchar(45),
-    ciudad varchar(30))
+	id varchar(45),
+    nomb varchar(45),
+    tel varchar(45),
+    ciu varchar(30))
 	BEGIN
-		declare id varchar(45);
-        declare nombre varchar(45);
-        declare tel varchar(45);
-        declare ciu varchar(30);
-		insert into clientes values (id_cliente,nombre_cliente,telefono,ciudad);
+		insert into clientes values (id,nomb,tel,ciu);
     END; $$
     
 DELIMITER ;
-/*-------------------------------------------------------------------------------*/
 
 call sp_insertar_cliente("111","Juan","879465","Armenia");
+call sp_insertar_cliente("222","Jose","879466","Cali");
+/*-------------------------------------------------------------------------------*/
+/*---------------------------AGREGAR PRODUCTO-----------------------------------*/
+ DELIMITER $$
+create procedure sp_insertar_producto(
+	id varchar(45),
+    nomb varchar(45),
+    val double,
+    marca varchar(100),
+    tip varchar (20),
+    prov varchar (45))
+	BEGIN
+		insert into productos values (id,nomb,val,marca,tip,prov);
+    END; $$
+    
+DELIMITER ;
+
+call sp_insertar_producto("555-000","PC Gamer","5000000","Asus ROG Desktop","Ordenador","000-001");
+call sp_insertar_producto("556-000","PC Gamer","5500000","Acer Predator 500 Laptop","Ordenador","000-002");
+/*-------------------------------------------------------------------------------*/
+/*---------------------------AGREGAR PROVEEDOR-----------------------------------*/
+ DELIMITER $$
+create procedure sp_insertar_proveedor(
+	id varchar(45),
+    raz varchar(45),
+    nomb varchar(45),
+    dir varchar(60),
+    ciu varchar(20),
+    tel varchar (30))
+	BEGIN
+		insert into proveedores values (id,raz,nomb,dir,ciu,tel);
+    END; $$
+    
+DELIMITER ;
+
+call sp_insertar_proveedor("000-001","Entregas Rapidas","Elvis Ioso","Calle x # xx-xx","Armenia","789456");
+call sp_insertar_proveedor("000-002","Entregas Super Rapidas","Susana Horia","Carrera x # xx-xx","Pereira","123456");
+/*-------------------------------------------------------------------------------*/
+/*---------------------------AGREGAR VENTAS-----------------------------------*/
+ DELIMITER $$
+create procedure sp_insertar_venta(
+	id varchar(45),
+    prod varchar(45),
+    fec datetime)
+	BEGIN
+		insert into ventas values (id,prod,fec);
+    END; $$
+    
+DELIMITER ;
+
+call sp_insertar_venta("111","555-000","2022-1-1");
+call sp_insertar_venta("222","556-000","2020-12-12");
+/*-------------------------------------------------------------------------------*/
+/*---------------------------AGREGAR ENTREGAS-----------------------------------*/
+ DELIMITER $$
+create procedure sp_insertar_entrega(
+	id_pro varchar(45),
+    id_prod varchar(45))
+	BEGIN
+		insert into entregas_inv values (id_pro,id_prod);
+    END; $$
+    
+DELIMITER ;
+
+call sp_insertar_entrega("000-001","555-000");
+call sp_insertar_entrega("000-002","556-000");
+/*-------------------------------------------------------------------------------*/
